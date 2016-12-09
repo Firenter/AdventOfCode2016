45c92e715e402486eeb25ecb95fd51562499513e
Firenter <cedriccorthout@msn.com>
2016-12-09 13:53:16 +0100

    Initial solution files

---
+++ AdventOfCode2016/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Go check it out right now, we\'ll see about doing it later!");
+            Console.ReadLine();
+        }
+    }
+}
